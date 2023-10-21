efaad9c88d43689e6cf90d113fcdd101427413be
sneha <snehnang@udel.edu>
2023-10-21 17:24:49 -0400

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -11,28 +11,27 @@ import {
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./Logo"
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
+    <Box textAlign="center" fontSize="xl" backgroundColor="#4d94ff">
+      <Text fontSize='4xl' as='b' color="#333333">MatchAbility</Text>
+      <Tabs variant='soft-rounded' colorScheme='red'>
+        <TabList>
+          <Tab color="#333333">Forms</Tab>
+          <Tab color="#333333">Our Volunteers</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <p>Match to a Volunteer</p>
+            <p>Become a Volunteer</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Person</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Box>
   </ChakraProvider>
 )
